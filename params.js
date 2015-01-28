1d8fbe933f79a5b246b2a462b93abf8b266753ba
Eric <eric.basley@redpelicans.com>
2015-01-28 21:13:35 +0100

    point mongo to host 'mongo'

--- params.js
+++ params.js
@@ -2,15 +2,13 @@ module.exports = {
   frontendApp: 'frontend',
   serverIp: '5.39.80.203',
   livereload: 35729,
-  http:{
-    port: 3003,
-  },
+  http:{ port: 80 },
   db: {
-    host: 'localhost',
+    host: 'mongo',
     port: 27017,
     database: 'transacs',
     auto_reconnect: true,
-    poolSize: 100, 
+    poolSize: 10, 
     w: 1, 
     strict: true, 
     native_parser: true
