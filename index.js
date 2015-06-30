05dac8424f90b6ad93e11d66442824ccd1776a2b
Eric <eric.basley@redpelicans.com>
2015-06-30 14:30:34 +0000

    fix ex6 index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import * as server from './dist/server';
-export function create(options, cb){
-  let promise = server.create(options);
+'use strict';
+
+var server = require('./dist/server');
+
+exports.create = create;
+
+function create(options, cb){
+  var promise = server.create(options);
   if(!cb) return promise;
-  else promise.then((app) => cb(null, app)).catch(err => cb(err));
+  else promise
+    .then(function(app){ cb(null, app) })
+    .catch(function(err){ cb(err) });
 }
